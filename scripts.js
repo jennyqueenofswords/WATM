b864687e6cc1a5215083f7978b689d93a66fe6ea
jennyqueenofswords <129200331+jennyqueenofswords@users.noreply.github.com>
2023-05-29 12:40:54 -0400

    fixed broken things

--- scripts.js
+++ scripts.js
@@ -5,7 +5,11 @@ const HEROKU_URL = "https://powerful-stream-53189.herokuapp.com/";
 document.body.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     const buttonId = event.target.id.split('-')[1];
-    processVote(buttonId);
+    if (buttonId === 'submit-poem') {
+      submitPoem();
+    } else if (buttonId === 'vote-human' || buttonId === 'vote-ai') {
+      processVote(buttonId);
+    }
   }
 });
 
@@ -41,23 +45,62 @@ const submitPoem = async () => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
+    const result = await response.json();
     animateSubmit();
     showModal("Poem submitted!");
+
+    // Display user's poem and AI-generated poem with critic review
+    const aiPoemSection = document.getElementById("ai-poem");
+    const aiGeneratedPoem = document.getElementById("ai-generated-poem");
+    const aiCriticReview = document.getElementById("ai-critic-review");
+    aiGeneratedPoem.textContent = `AI-generated poem: ${result.ai_poem}`;
+    aiCriticReview.textContent = `AI critic's review: ${result.ai_review}`;
+    aiPoemSection.style.display = "block";
+    
+    // Save user's poem to database
+    const nameInput = document.querySelector("#poem-submission-form input[name='name']");
+    const linkInput = document.querySelector("#poem-submission-form input[name='link']");
+    const name = nameInput.value;
+    const link = linkInput.value;
+    const randomWords = result.random_words;
+    const userPoem = { poem: escapedPoem, name: name, link: link, randomWords: randomWords };
+    const saveResponse = await fetch(`${HEROKU_URL}/savePoem`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userPoem),
+    });
+    if (!saveResponse.ok) {
+      throw new Error(`HTTP error! status: ${saveResponse.status}`);
+    }
   } catch (error) {
     console.error("Submission error:", error);
     showModal("Failed to submit poem!");
   }
 };
 
-// Function to display AI poem
-const displayAiPoem = (poem) => {
+// Function to display poems for voting
+const displayVotingPoems = (poems) => {
+  const humanPoem = poems[0];
+  const aiPoem = poems[1];
+  const humanPoemSection = document.getElementById("human-poem");
   const aiPoemSection = document.getElementById("ai-poem");
-  const aiGeneratedPoem = document.getElementById("ai-generated-poem");
-  const aiCriticReview = document.getElementById("ai-critic-review");
+  const humanPoemText = document.getElementById("human-poem-text");
+  const aiPoemText = document.getElementById("ai-poem-text");
+  const humanPoemAuthor = document.getElementById("human-poem-author");
+  const aiPoemAuthor = document.getElementById("ai-poem-author");
+  const humanVoteBtn = document.getElementById("vote-human");
+  const aiVoteBtn = document.getElementById("vote-ai");
 
-  aiGeneratedPoem.textContent = `AI-generated poem: ${poem.ai_poem}`;
-  aiCriticReview.textContent = `AI critic's review: ${poem.ai_review}`;
+  humanPoemText.textContent = humanPoem.poem;
+  humanPoemAuthor.textContent = `By ${humanPoem.name}`;
+  aiPoemText.textContent = aiPoem.poem;
+  aiPoemAuthor.textContent = `By AI`;
+  humanPoemSection.style.display = "block";
   aiPoemSection.style.display = "block";
+  humanVoteBtn.dataset.poemId = humanPoem._id;
+  aiVoteBtn.dataset.poemId = aiPoem._id;
 };
 
 // Function to process vote
@@ -65,10 +108,11 @@ const processVote = async (vote) => {
   console.log(`Processing vote for Poem ${vote}`);
 
   try {
+    const poemId = event.target.dataset.poemId;
     const response = await fetch(`${HEROKU_URL}/send_vote`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ vote: vote })
+      body: JSON.stringify({ vote: vote, poemId: poemId })
     });
 
     if (!response.ok) {
@@ -77,6 +121,14 @@ const processVote = async (vote) => {
 
     const result = await response.json();
     console.log(result.message);
+
+    // Display authorship and critic's choice
+    const humanPoemAuthor = document.getElementById("human-poem-author");
+    const aiPoemAuthor = document.getElementById("ai-poem-author");
+    const criticChoice = document.getElementById("critic-choice");
+    humanPoemAuthor.textContent = `By ${result.human_poem.name}`;
+    aiPoemAuthor.textContent = `By AI`;
+    criticChoice.textContent = `Critic's choice: ${result.critic_choice}`;
   } catch (error) {
     console.error("Error sending vote:", error);
   }
@@ -97,11 +149,6 @@ const fetchRandomWords = async () => {
     console.error("Error fetching random words:", error);
   }
 };
-
-// Call the function in the writing-mode.html page
-if (document.getElementById("random-words-container")) {
-  fetchRandomWords();
-}
 
 // Function to animate submit button
 const animateSubmit = () => {
@@ -137,3 +184,27 @@ const closeModal = (element) => {
     document.body.removeChild(modal);
   }
 };
+
+// Call the function in the writing-mode.html page
+if (document.getElementById("random-words-container")) {
+  fetchRandomWords();
+}
+
+// Call the function in the voting-mode.html page
+if (document.getElementById("voting-container")) {
+  const fetchPoems = async () => {
+    try {
+      const response = await fetch(`${HEROKU_URL}/getPoems`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      displayVotingPoems(result.poems);
+    } catch (error) {
+      console.error("Error fetching poems:", error);
+    }
+  };
+  fetchPoems();
+}
